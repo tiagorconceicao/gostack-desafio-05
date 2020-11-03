a6401d9bae24114cf90b7177f44d71920360e68f
Tiago Ramos Conceicao <tiracotech@gmail.com>
2020-11-03 18:53:27 -0300

    add solution

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +19,57 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
+  private getIncome(): number {
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const income = incomeTransactions.reduce((accumulator, transaction) => {
+      return accumulator + transaction.value;
+    }, 0);
+
+    return income;
+  }
+
+  private getOutcome(): number {
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const outcome = incomeTransactions.reduce((accumulator, transaction) => {
+      return accumulator + transaction.value;
+    }, 0);
+
+    return outcome;
+  }
+
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.getIncome();
+    const outcome = this.getOutcome();
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const balance = this.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('This outcome value is not available');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
